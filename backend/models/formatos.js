848aaaad104c7e092f4737dfe14b8d38d5797128
mxmdg <91764900+mxmdg@users.noreply.github.com>
2023-08-08 23:45:35 -0300

    0.0.967
    Cambios en el backend... explorando las posibilidades de mongoose.

--- backend/models/formatos.js
+++ backend/models/formatos.js
@@ -2,10 +2,22 @@ const { Schema, model } = require('mongoose');
 
 const formatoSchema = new Schema(
     { 
-        'Nombre': {type: String, required: true},
+        'Nombre': {type: String, default: (this.Ancho + " x " + this.Alto),required: true},
         'Ancho': {type: Number, required: true},
         'Alto': {type: Number, required: true},
-    });
+    },
+    {
+        virtuals: {
+            Superficie: {
+                get() {
+                    return (this.Ancho * this.Alto)
+                }
+            }
+        }
+    }, {
+        toJSON: { virtuals: true } // <-- include virtuals in `JSON.stringify()`
+      }
+    );
 
 module.exports.esquema = model('Formatos', formatoSchema);
 module.exports.clase = formatoSchema;
--- backend/models/jobParts.js
+++ backend/models/jobParts.js
@@ -5,7 +5,13 @@ const jobPartSchema = new Schema(
         'Type': {type: String, required: true},
         'minPages': {type: Number, required: true},
         'maxPages': {type: Number, required: true},
-        'PrintModAllowed': {type: String, required: true},
+        'PrintModAllowed': {
+            type: String,
+            enum: ['simplex', 'duplex'],
+            required: function() {
+              return this.bacon > 3;
+            }
+          },
         'minStockWeight': {type: Number, required: true},
         'maxStockWeight': {type: Number, required: true},
         'Fecha': {type: Date, default: Date.now},
