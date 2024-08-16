683897b5bab8bbaab1a697748bf57ffc8c53057b
mxmdg <91764900+mxmdg@users.noreply.github.com>
2024-08-16 00:22:41 -0300

    0.1.0255

--- backend/routes/controllers/materialControl.js
+++ backend/routes/controllers/materialControl.js
@@ -1,58 +1,106 @@
-const materials = require('../../models/materiales')
+const materials = require("../../models/materiales");
 const prices = require("../../models/prices");
 
-const materialControl= {}
+const materialControl = {};
 
-materialControl.getMaterials = async (req,res) => {{
-    const material = await materials.esquema.find()
-    .populate({ path: "Precio_x_Kilo", model: prices.esquema });
-    
+materialControl.getMaterials = async (req, res) => {
+  {
+    const material = await materials.esquema.find();
+    //.populate({ path: "Precio_x_Kilo", model: prices.esquema });
+
     res.json(material);
-}}
+  }
+};
 
-materialControl.addMaterial = async (req,res)=>{{
-     try {
-        const {Nombre_Material, Marca, Gramaje, Tipo, Ancho_Resma, Alto_Resma, Espesor_Resma, Fibra, Precio_x_Kilo, Color} = req.body;
-        const newMaterial = new materials.esquema({Nombre_Material, Marca, Gramaje, Tipo, Ancho_Resma, Alto_Resma, Espesor_Resma, Fibra, Precio_x_Kilo, Color});
-        await newMaterial.save();
-        res.json({"message": newMaterial.Nombre + " guardado OK"});
+materialControl.addMaterial = async (req, res) => {
+  {
+    try {
+      const {
+        Nombre_Material,
+        Marca,
+        Gramaje,
+        Tipo,
+        Ancho_Resma,
+        Alto_Resma,
+        Espesor_Resma,
+        Fibra,
+        Precio_x_Kilo,
+        Color,
+      } = req.body;
+      const newMaterial = new materials.esquema({
+        Nombre_Material,
+        Marca,
+        Gramaje,
+        Tipo,
+        Ancho_Resma,
+        Alto_Resma,
+        Espesor_Resma,
+        Fibra,
+        Precio_x_Kilo,
+        Color,
+      });
+      await newMaterial.save();
+      res.json({ message: newMaterial.Nombre + " guardado OK" });
     } catch (e) {
-        console.log(e)
-        res.status(404).json({ message: e });
-      }
-    
-    
-    
-}}
-
-materialControl.getMaterial = async (req, res)=> {
-    try {
-        const material = await materials.esquema.findById(req.params.id)
-        .populate({ path: "Precio_x_Kilo", model: prices.esquema });
-        if (material) {
-            res.json(material)
-        } else {
-            res.status(404).json({ message: "Material no encontrado" });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Error al obtener el Material" });
+      console.log(e);
+      res.status(404).json({ message: e });
     }
-}
-    
-materialControl.updateMaterial = async (req, res)=> {
-    try {
-        const {Nombre_Material, Marca, Gramaje, Tipo, Ancho_Resma, Alto_Resma, Espesor_Resma, Fibra, Precio_x_Kilo, Color} = req.body;
-        const material = await materials.esquema.findOneAndUpdate({ _id: req.params.id },{Nombre_Material, Marca, Gramaje, Tipo, Ancho_Resma, Alto_Resma, Espesor_Resma, Fibra, Precio_x_Kilo, Color})
-        res.json({"Message": "Material actualizado " + material.Nombre})
-    } catch (error) {
-        console.log("Error: " + error)
-        res.status(404).json({error})
-    }
-}
-materialControl.deleteMaterial = async (req, res)=> {
-    const material =  await materials.esquema.findByIdAndDelete(req.params.id);
-    res.json({"Message": "Material eliminado"});
-}
+  }
+};
 
-  module.exports = materialControl
+materialControl.getMaterial = async (req, res) => {
+  try {
+    const material = await materials.esquema.findById(req.params.id);
+    //.populate({ path: "Precio_x_Kilo", model: prices.esquema });
+    if (material) {
+      res.json(material);
+    } else {
+      res.status(404).json({ message: "Material no encontrado" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error al obtener el Material" });
+  }
+};
+
+materialControl.updateMaterial = async (req, res) => {
+  try {
+    const {
+      Nombre_Material,
+      Marca,
+      Gramaje,
+      Tipo,
+      Ancho_Resma,
+      Alto_Resma,
+      Espesor_Resma,
+      Fibra,
+      Precio_x_Kilo,
+      Color,
+    } = req.body;
+    const material = await materials.esquema.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        Nombre_Material,
+        Marca,
+        Gramaje,
+        Tipo,
+        Ancho_Resma,
+        Alto_Resma,
+        Espesor_Resma,
+        Fibra,
+        Precio_x_Kilo,
+        Color,
+      }
+    );
+    res.json({ Message: "Material actualizado " + material.Nombre });
+  } catch (error) {
+    console.log("Error: " + error);
+    res.status(404).json({ error });
+  }
+};
+materialControl.deleteMaterial = async (req, res) => {
+  const material = await materials.esquema.findByIdAndDelete(req.params.id);
+  res.json({ Message: "Material eliminado" });
+};
+
+module.exports = materialControl;
