0319c1dd2c19e679630b93bf88b0af80b081c861
mxmdg <91764900+mxmdg@users.noreply.github.com>
2025-12-14 13:25:33 -0300

    0.1.076

--- frontend/src/Components/Config/config.js
+++ frontend/src/Components/Config/config.js
@@ -1,6 +1,7 @@
-export const url = "http://181.104.19.45:";
-//export const url = "http://192.168.0.198:";
-// export const url = "http://192.168.56.1:";
+//export const url = "http://181.104.19.45:";
+export const url = "http://192.168.0.198:";
+//export const url = "http://192.168.56.1:";
+//export const url = "http://192.168.0.130:";
 export const serverURL = url + "3000";
 export const databaseURL = url + "5000/hamlet/";
 export const VALIDATE_PDF = url + "8000/upload";
