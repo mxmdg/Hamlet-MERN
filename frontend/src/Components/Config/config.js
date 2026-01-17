9b8169e289afdfe100fae9794c443da3dec02673
mxmdg <91764900+mxmdg@users.noreply.github.com>
2026-01-17 16:39:21 -0300

    0.0195
    Nginx config with ssl

--- frontend/src/Components/Config/config.js
+++ frontend/src/Components/Config/config.js
@@ -2,9 +2,8 @@
 //export const url = "http://192.168.0.198:";
 //export const url = "http://192.168.56.1:";
 //export const url = "http://192.168.0.137:";
-export const API_BASE = `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}`;
+export const API_BASE = process.env.REACT_APP_API_HOST; // https://www.hamlet.com.ar
 
-// módulos
 export const HAMLET_API = `${API_BASE}/Hamlet/`;
-export const HEALTH_API = `${API_BASE}/health`;
-export const VALIDATE_PDF = `${process.env.REACT_APP_API_HOST}:8000/upload`;
+export const HEALTH_API = `${API_BASE}/health/`; // Asegúrate que termine en /
+export const VALIDATE_PDF = `${API_BASE}/upload`;
